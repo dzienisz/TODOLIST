5afadb8b1a292c05657b4aee085faf8e72df11a2
Kamil Dzieniszewski <kamil.dzieniszewski@gmail.com>
2018-03-26 15:59:27 +0200

    initial state ✍🏻

--- src/App.js
+++ src/App.js
@@ -6,13 +6,17 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>React Redux App</h1>
+        <form>
+          <input
+            placeholder="Enter text"
+          />
+          <button
+            type="button"
+          >
+            Button
+          </button>
+        </form>
       </div>
     );
   }
