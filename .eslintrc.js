01ab8034ff2be0747a48df8154dc0e4dad20c34f
Kamil Dzieniszewski <kamil.dzieniszewski@gmail.com>
2018-03-27 16:40:07 +0200

    eslint fixes 😝

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,9 @@
 module.exports = {
-    "extends": "airbnb"
+    "extends": "airbnb",
+    "plugins": ["prettier"],
+    "rules": {
+        "prettier/prettier": "error"
+    },
+    "jsx-a11y/href-no-hash": "off",
+    "jsx-a11y/anchor-is-valid": ["warn", { "aspects": ["invalidHref"] }]
 };
