6554aef174b0b229cfade9846b63d7fec77e3e37
Kamil Dzieniszewski <kamil.dzieniszewski@daftcode.pl>
2019-02-07 23:03:23 +0100

    Updates

---
+++ precache-manifest.3399ee5546f33d5bf27ffc6573e25baf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5db3f93e30604ec76395",
+    "url": "/TODOLIST/static/css/main.56dc8a66.chunk.css"
+  },
+  {
+    "revision": "5db3f93e30604ec76395",
+    "url": "/TODOLIST/static/js/main.5db3f93e.chunk.js"
+  },
+  {
+    "revision": "4d7d385487c1031b1c4b",
+    "url": "/TODOLIST/static/js/1.4d7d3854.chunk.js"
+  },
+  {
+    "revision": "503788b606365cedb9ef",
+    "url": "/TODOLIST/static/js/runtime~main.503788b6.js"
+  },
+  {
+    "revision": "7e02360417858f7ffab2bed9426027d1",
+    "url": "/TODOLIST/index.html"
+  }
+];
