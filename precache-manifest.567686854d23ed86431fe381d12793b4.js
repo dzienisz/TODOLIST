1d5c52746bba7243ffe9f4dd1be302abd4646331
Kamil Dzieniszewski <kamil.dzieniszewski@vodeno.com>
2019-09-08 18:56:23 +0200

    Updates

---
+++ precache-manifest.567686854d23ed86431fe381d12793b4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77d4c3cc483c126f33a60369e9f5e58b",
+    "url": "/TODOLIST/index.html"
+  },
+  {
+    "revision": "ab9d8740d5573b5ba6f3",
+    "url": "/TODOLIST/static/css/main.d8d15c73.chunk.css"
+  },
+  {
+    "revision": "fa4d102dfe981fda00eb",
+    "url": "/TODOLIST/static/js/2.f57b1de3.chunk.js"
+  },
+  {
+    "revision": "ab9d8740d5573b5ba6f3",
+    "url": "/TODOLIST/static/js/main.3a6e37e2.chunk.js"
+  },
+  {
+    "revision": "faf0792412354e6e026f",
+    "url": "/TODOLIST/static/js/runtime~main.b2c0b52f.js"
+  }
+]);
